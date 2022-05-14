0a493b6dbd020802132c93a3b1db656524801a0c
harveen <harveensinghchadha@gmail.com>
2022-05-15 04:54:19 +0530

    Adding WebRTCVAD

--- src/vadx.py
+++ src/vadx.py
@@ -1,26 +1,6 @@
 
 
 
-class BaseVad(object):
-    def get_timestamps():
-        pass
-
-    def save_audio_chunks():
-        pass
-
-    def split(self, file_paths, **kwargs):
-        print("Inside split method")
-        pass
-
-    def save_broken_files(blank_offset_duration):
-        pass
-
-    def get_broken_files(self, blank_offset_duration):
-        pass
-
-    def get_silence_removed_single_file(self, blank_offset_duration):
-        pass
-
 
 
 # class WebRTCVAD(BaseVad):
--- src/webrtc/pywebrtc.py
+++ src/webrtc/pywebrtc.py
@@ -127,6 +127,28 @@ def create_new_vad_object():
     print("I am here")
 
 
+class BaseVad(object):
+    def get_timestamps():
+        pass
+
+    def save_audio_chunks():
+        pass
+
+    def split(self, file_paths, **kwargs):
+        print("Inside split method")
+        pass
+
+    def save_broken_files(blank_offset_duration):
+        pass
+
+    def get_broken_files(self, blank_offset_duration):
+        pass
+
+    def get_silence_removed_single_file(self, blank_offset_duration):
+        pass
+
+
+
 
 class WebRTCClass(BaseVad):
     def __init__(self):
